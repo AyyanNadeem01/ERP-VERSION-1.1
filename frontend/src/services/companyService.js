d8d2749ebf2b5bc277a70870a64b34b79dc0ceb9
Ayyan Nadeem <ayyan137a@gmail.com>
2025-08-18 17:25:27 +0500

    Update companyService.js

--- frontend/src/services/companyService.js
+++ frontend/src/services/companyService.js
@@ -1,55 +1,22 @@
-// // src/services/companyService.js
-// import axiosInstance from "./axiosInstance"; 
-// const createCompany = async (data) => {
-//   const res = await axiosInstance.post("/company", data);
-//   return res.data;
-// };
-
-// const getMyCompany = async () => {
-//   const res = await axiosInstance.get("/company");
-//   return res.data;
-// };
-
-// const updateCompany = async (data) => {
-//   const res = await axiosInstance.put("/company", data);
-//   return res.data;
-// };
-
-// const deleteCompany = async () => {
-//   const res = await axiosInstance.delete("/company");
-//   return res.data;
-// };
-
-// const companyService = {
-//   createCompany,
-//   getMyCompany,
-//   updateCompany,
-//   deleteCompany,
-// };
-
-// export default companyService;
-
-
 // src/services/companyService.js
-import axiosInstance from "./axiosInstance";
-
+import axiosInstance from "./axiosInstance"; 
 const createCompany = async (data) => {
-  const res = await axiosInstance.post("/api/company", data);
+  const res = await axiosInstance.post("/company", data);
   return res.data;
 };
 
 const getMyCompany = async () => {
-  const res = await axiosInstance.get("/api/company");
+  const res = await axiosInstance.get("/company");
   return res.data;
 };
 
 const updateCompany = async (data) => {
-  const res = await axiosInstance.put("/api/company", data);
+  const res = await axiosInstance.put("/company", data);
   return res.data;
 };
 
 const deleteCompany = async () => {
-  const res = await axiosInstance.delete("/api/company");
+  const res = await axiosInstance.delete("/company");
   return res.data;
 };
 
@@ -61,3 +28,6 @@ const companyService = {
 };
 
 export default companyService;
+
+
+src/services/companyService.js
