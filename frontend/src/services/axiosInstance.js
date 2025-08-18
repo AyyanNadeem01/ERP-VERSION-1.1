de0af386350cdb61b84b42b70c4923dc26f35691
Ayyan Nadeem <ayyan137a@gmail.com>
2025-08-18 17:26:36 +0500

    Update axiosInstance.js

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,13 +1,33 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: process.env.REACT_APP_BACKEND_URL,
+// });
+
+// // Attach token to every request
+// axiosInstance.interceptors.request.use(
+//   (config) => {  console.log(localStorage.getItem("token"));
+
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers["Authorization"] = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default axiosInstance;
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`, // ✅ add /api here
 });
 
 // Attach token to every request
 axiosInstance.interceptors.request.use(
-  (config) => {  console.log(localStorage.getItem("token"));
-
+  (config) => {
+    console.log(localStorage.getItem("token"));
     const token = localStorage.getItem("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
